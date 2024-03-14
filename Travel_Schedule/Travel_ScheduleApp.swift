14416b7c05938457aa582f28f1904bc948557b5a
Yan <janmaximov94@yandex.ru>
2024-03-14 15:36:17 +0300

    Initial Commit

---
+++ Travel_Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel_Schedule
+//
+//  Created by Ян Максимов on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
